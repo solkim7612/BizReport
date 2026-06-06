e538f3e7e258db176c753bbeb885e306ac3bb358
solkim7612 <kssol626@gmail.com>
2026-06-06 19:17:04 +0900

    finish

--- api/src/main/java/com/bizreport/api/ApiApplication.java
+++ api/src/main/java/com/bizreport/api/ApiApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.bizreport")
 @EntityScan(basePackages = "com.bizreport")
 @EnableJpaRepositories(basePackages = "com.bizreport")
+@EnableScheduling
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
